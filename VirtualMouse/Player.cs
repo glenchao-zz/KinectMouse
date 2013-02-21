90c0d7b24a46b3802e444c0a43efa5684681ebe9
Glen Chao <glenchao@outlook.com>
2013-02-21 13:40:04 -0500

    Lots of stuff, read description
    . Added toolkit
    . Deleted colorframe, skeletonframe, depthframe --> added allframes
    . Changed to only draw area around hand
    .  Added simple player class (that doesn't do anything yet)

---
+++ VirtualMouse/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VirtualMouse
+{
+    class Player
+    {
+        public Player(int index)
+        {
+            this.playerIndex = index;
+        }
+
+        public int playerIndex { get; set; }
+    }
+}
