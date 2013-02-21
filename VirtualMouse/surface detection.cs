d0eec40ad1a5001224277f1d72ffad728cf72d9c
Namal Rajatheva <namal9098@gmail.com>
2013-02-21 15:14:53 -0500

    Useless addition

---
+++ VirtualMouse/surface detection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VirtualMouse
+{
+    class surface_detection
+    {
+        private Array Surface(bitmap, skeletonframe)
+        {
+            
+        
+        }
+    }
+}
